109c28ece5536ca822553a3ae8dc793c5e4f78c9
emsi93 <krejpowiczmichal@gmail.com>
2018-03-07 21:19:24 +0100

    poprawki

--- src/main/java/com/parcel/distribution/ParcelDistributionApplication.java
+++ src/main/java/com/parcel/distribution/ParcelDistributionApplication.java
@@ -1,11 +1,19 @@
 package com.parcel.distribution;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaRepositories("com.parcel.distribution.db.repository")
+@Configuration
+@EnableAutoConfiguration
 @ComponentScan({"com.parcel.distribution"})
-@SpringBootApplication
+@EntityScan("com.parcel.distribution.db.entity")
+@EnableScheduling
 public class ParcelDistributionApplication {
 
 	public static void main(String[] args) {
