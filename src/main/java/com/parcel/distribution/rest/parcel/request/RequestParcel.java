80f89966de47a4aeb1eeeef8e9ff8d691d878fed
emsi93 <krejpowiczmichal@gmail.com>
2018-09-09 16:00:21 +0200

    delete test

--- src/main/java/com/parcel/distribution/rest/parcel/request/RequestParcel.java
+++ src/main/java/com/parcel/distribution/rest/parcel/request/RequestParcel.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class RequestParcel {
 
-//    private String login;
-//    private String token;
+    private String login;
+    private String token;
     private String idParcel;
     private String code;
 }
